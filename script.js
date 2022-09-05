80bd797457b294b5e35b329577982f1cb8d711b7
Rohit Rajendra Barate <rohitbarate100@gmail.com>
2022-09-05 14:52:30 +0530

    navbar

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 let menuBtn = document.getElementById("menu-btn");
-let navbar = document.getElementById("navbar");
+let navbar = document.getElementById("header");
 
-function clickHandler(){
-    if(menuBtn.classList.contains("active")){
-        menuBtn.classList.toggle("non-active");
-        menuBtn.classList.remove("active");
-        navbar.style.display="none"
-        
-    }else{
-        menuBtn.classList.toggle("active");
-        menuBtn.classList.remove("non-active");
-        navbar.style.display="flex"
+function clickHandler() {
+    console.log(window.innerWidth);
+    if (window.innerWidth <= 1000) {
+        if (menuBtn.classList.contains("active")) {
+            menuBtn.classList.toggle("non-active");
+            menuBtn.classList.remove("active");
+            navbar.style.height = "10vh";
+
+        } else {
+            menuBtn.classList.toggle("active");
+            menuBtn.classList.remove("non-active");
+            navbar.style.height = "max-content"
+
+        }
 
     }
 
